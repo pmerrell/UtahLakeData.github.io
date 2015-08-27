7ba1bbcae0206d4aad2deef46eb7213853174bd3
pmerrell <prestonmerrell@gmail.com>
2015-08-27 14:21:40 -0600

    Update map.js

--- map.js
+++ map.js
@@ -42,7 +42,7 @@ var map = new ol.Map({
 	overlays: [overlay],
 	target: 'map',
 	view: new ol.View({
-		  center: ol.proj.transform([-81.36191579883884, 41.6251840640563], 'EPSG:4326', projection),
+		  center: ol.proj.transform([ 40.204301, -111.823190], 'EPSG:4326', projection),
 		  zoom: 5
 	})
 });
